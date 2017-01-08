648aa8faa4ede0f508171b98ba52a7c57a6a602e
Balhau <balhau@balhau.net>
2017-01-08 17:14:51 +0000

    Added more on UI

--- app/src/main/java/net/balhau/android/byts/fragments/TorrentsFragment.java
+++ app/src/main/java/net/balhau/android/byts/fragments/TorrentsFragment.java
@@ -18,6 +18,7 @@ public class TorrentsFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_byts_torrent,container,false);
+        View torrentsView = inflater.inflate(R.layout.fragment_byts_torrent,container,false);
+        return torrentsView;
     }
 }
---
+++ app/src/main/java/net/balhau/android/byts/fragments/details/TorrentsDetail.java
@@ -0,0 +1,10 @@
+package net.balhau.android.byts.fragments.details;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by vitorfernandes on 1/8/17.
+ */
+
+public class TorrentsDetail extends Fragment {
+}
